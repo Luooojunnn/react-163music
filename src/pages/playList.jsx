fb4cf6c0f8c7afc379a6716b52a865d809116e82
luojuna <luojuna@jiedaibao.com>
2019-10-11 11:17:12 +0800

    test:测试featch接口

--- src/pages/playList.jsx
+++ src/pages/playList.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import { fetch } from "../utils/fetch";
+
+
 class PlayList extends React.Component {
   componentDidMount(){
     console.log(this.props)
+    fetch(`playlist/detail?id=2920471281`).then(r=>console.log(r))
   }
   render(){
     return (
